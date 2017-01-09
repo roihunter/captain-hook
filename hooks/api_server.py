01dd7fcf4605a93f70cf735c2c9555b4804bd71f
Mišo Belica <miso.belica@gmail.com>
2017-01-09 15:41:29 +0100

    Added hack for our allani.pl client and his images #11785

--- hooks/api_server.py
+++ hooks/api_server.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import re
+from urllib.parse import quote
+
 import falcon
 import pika.exceptions
 import requests
@@ -46,6 +49,8 @@ class FacebookHooks:
 
 
 class CorsProxy:
+    _ALLANI_PL_PATTERN = re.compile(r"(?P<prefix>http://st\.allani\.pl/.+/)(?P<url>http.+)$", re.UNICODE)
+
     def __init__(self):
         self._logger = services.logger()
 
@@ -58,6 +63,7 @@ class CorsProxy:
             resp.set_header("access-control-allow-headers", allow_headers)
 
         url = req.relative_uri.replace(_API_PREFIX + "/proxy/", "")
+        url = self._fix_exceptional_urls(url)
 
         try:
             self._logger.info("Trying to proxy URL: %s", url)
@@ -72,6 +78,18 @@ class CorsProxy:
         except:
             self._logger.exception("Not able to proxy URL: %s", url)
             resp.status = falcon.HTTP_400
+
+    def _fix_exceptional_urls(self, url):
+        """
+        This is here because we were not able to set this working by nginx so this is hack to handle our client properly.
+        See http://is.roihunter.com/issues/11785#note-22 and related tasks for more info.
+        """
+        match = self._ALLANI_PL_PATTERN.match(url)
+
+        if match:
+            return match.group("prefix") + quote(match.group("url"), safe="")
+        else:
+            return url
 
 
 class API(falcon.API):
