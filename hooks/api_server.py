c06a83754d726ed75bec8d3fb89a9e8af66b92d1
Joozty <32jojo32@gmail.com>
2017-12-21 11:32:35 +0100

    Remove old CORS proxy #19954

--- hooks/api_server.py
+++ hooks/api_server.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import re
-from urllib.parse import quote
-
 import falcon
 import pika.exceptions
 import requests
@@ -50,21 +47,12 @@ class FacebookHooks:
 
 class CorsProxy:
     _DEFAULT_USER_AGENT = "ROI Hunter/CORS proxy; http://roihunter.com/"
-    _ALLANI_PL_PATTERN = re.compile(r"(?P<prefix>https?://st\.allani\.pl/.+/)(?P<url>http.+)$", re.UNICODE)
-    _PRODUCTSUP_IO_PATTERN = re.compile(r"(?P<prefix>https?://[^.]+\.productsup\.io/img/site/\d+/data/)(?P<data>[^?#]+)(?P<suffix>.*)$", re.UNICODE)
     _TIMEOUT_IN_SECONDS = (10, 20)
 
     def __init__(self):
         self._logger = services.logger()
 
     def on_get(self, req, resp):
-        self(req, resp)
-
-    def __call__(self, req, resp):
-        """
-        This is obsolete and should be removed once proxy with "?url=..." will be used everywhere.
-        This code should be moved to method "on_get" and method "__call__" deleted.
-        """
         resp.set_header("access-control-allow-origin", "*")
         resp.set_header("access-control-allow-methods", "GET")
         resp.set_header("access-control-max-age", "21600")
@@ -73,9 +61,6 @@ class CorsProxy:
             resp.set_header("access-control-allow-headers", allow_headers)
 
         url = req.get_param("url")
-        if not url:  # FIXME: this block is obsolete and should be removed once proxy with "?url=..." will be used everywhere
-            url = req.relative_uri.replace(_API_PREFIX + "/proxy/", "")
-            url = self._fix_exceptional_urls(url)
 
         try:
             self._logger.info("Trying to proxy URL: %s", url)
@@ -92,23 +77,6 @@ class CorsProxy:
             self._logger.exception("Not able to proxy URL: %s", url)
             resp.status = falcon.HTTP_400
 
-    def _fix_exceptional_urls(self, url):
-        """
-        This is here because we were not able to set this working by nginx so this is hack to handle our client properly.
-        See http://is.roihunter.com/issues/11785#note-22 and related tasks for more info.
-
-        FIXME: this is obsolete and should be removed once proxy with "?url=..." will be used everywhere
-        """
-        match = self._ALLANI_PL_PATTERN.match(url)
-        if match:  # https://is.roihunter.com/issues/11785
-            return match.group("prefix") + quote(match.group("url"), safe="")
-
-        match = self._PRODUCTSUP_IO_PATTERN.match(url)
-        if match:  # https://is.roihunter.com/issues/19496
-            return match.group("prefix") + quote(match.group("data"), safe="") + match.group("suffix")
-
-        return url
-
     @classmethod
     def _get_user_agent(cls, api_request):
         user_agent = api_request.headers.get("USER-AGENT")
@@ -124,4 +92,3 @@ class API(falcon.API):
         self.add_route("/health", Health())
         self.add_route(_API_PREFIX + "/{scope}/fb/hooks", FacebookHooks())
         self.add_route(_API_PREFIX + "/proxy", CorsProxy())
-        self.add_sink(CorsProxy(), _API_PREFIX + "/proxy/")  # FIXME: this is obsolete and should be removed once proxy with "?url=..." will be used everywhere
