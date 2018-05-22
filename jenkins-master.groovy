9ccb3de00d4865f00c27184072444582b9c9f21e
Daniil Svirin <daniil.svirin@roihunter.com>
2018-05-22 12:25:45 +0200

    Kubernetes deployment prepared

--- jenkins-master.groovy
+++ jenkins-master.groovy
@@ -57,43 +57,27 @@ pipeline {
 
         stage('Deploy API container') {
             steps {
-                withCredentials([
-                    string(credentialsId: 'docker-registry-azure', variable: 'DRpass'),
-                    string(credentialsId: "captainhook-master-facebook-verify-token", variable: "captainhook_facebook_verify_token" ),
-                    usernamePassword(
-                        credentialsId: "captainhook-master-rabbit-username-password",
-                        passwordVariable: "captainhook_rabbit_password",
-                        usernameVariable: "captainhook_rabbit_username"
+                withCredentials([file(credentialsId: 'jenkins-roihunter-master-kubeconfig', variable: 'kube_config')]) {
+                    kubernetesDeploy(
+                        configs: '**/kubernetes/captain-hook-master-deployment.yaml,**/kubernetes/captain-hook-master-service.yaml',
+                        dockerCredentials: [
+                             [credentialsId: 'docker-azure-credentials', url: 'http://roihunter.azurecr.io']
+                        ],
+                        kubeConfig: [
+                            path: "$kube_config"
+                        ],
+                        secretName: 'regsecret',
+                        ssh: [
+                            sshCredentialsId: '*',
+                            sshServer: ''
+                        ],
+                        textCredentials: [
+                            certificateAuthorityData: '',
+                            clientCertificateData: '',
+                            clientKeyData: '',
+                            serverUrl: 'https://'
+                        ]
                     )
-                ]) {
-                    script {
-                        def servers = params.APP_SERVERS.tokenize(',')
-
-                        for (item in servers) {
-                            sshagent(['5de2256c-107d-4e4a-a31e-2f33077619fe']) {
-                                sh """ssh -oStrictHostKeyChecking=no -t -t jenkins@${item} <<EOF
-                                    docker login roihunter.azurecr.io -u roihunter -p "$DRpass"
-                                    docker pull roihunter.azurecr.io/captain-hook/master
-                                    docker stop captain-hook-master; true
-                                    docker rm -v captain-hook-master; true
-                                    docker run --detach -p 8007:8005 \
-                                        -e "GUNICORN_BIND=0.0.0.0:8005" \
-                                        -e "GUNICORN_WORKERS=4" \
-                                        -e "CAPTAINHOOK_PROFILE=master" \
-                                        -e "CAPTAINHOOK_FACEBOOK_VERIFY_TOKEN=${captainhook_facebook_verify_token}" \
-                                        -e "CAPTAINHOOK_RABBIT_LOGIN=${captainhook_rabbit_username}" \
-                                        -e "CAPTAINHOOK_RABBIT_PASSWORD=${captainhook_rabbit_password}" \
-                                        -e "CAPTAINHOOK_RABBIT_HOST=${params.RABBIT_HOST}" \
-                                        -e "CAPTAINHOOK_RABBIT_PORT=${params.RABBIT_PORT}" \
-                                        --hostname=captain-hook-master-${item} \
-                                        --name=captain-hook-master \
-                                        --restart=always \
-                                        roihunter.azurecr.io/captain-hook/master
-                                    exit
-                                    EOF"""
-                            }
-                        }
-                    }
                 }
             }
         }
